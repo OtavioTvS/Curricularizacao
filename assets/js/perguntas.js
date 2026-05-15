907d8cbb35fa837d709d1a20c080cbaf251f0c12
Otavio Henrique da Silva <otavio.silva@unifebe.edu.br>
2026-05-15 17:08:46 -0300

    Add files via upload
    Final?

---
+++ assets/js/perguntas.js
@@ -0,0 +1,17 @@
+export const perguntas = [
+    "pergunta1",
+    "pergunta2",
+    "pergunta3",
+    "pergunta4",
+    "pergunta5",
+    "pergunta6",
+    "pergunta7",
+    "pergunta8",
+    "pergunta9",
+    "pergunta10",
+    "pergunta11",
+    "pergunta12",
+    "pergunta13",
+    "pergunta14",
+    "pergunta15"
+];
